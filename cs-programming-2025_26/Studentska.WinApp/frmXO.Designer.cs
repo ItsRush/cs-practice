41781229ce191fed79e5aa45a3e97363f35c23bb
Lejla Hotic <lejlahotic3@gmail.com>
2026-01-29 01:00:42 +0100

    Add actual project files to cs-programming-2025_26 folder

---
+++ cs-programming-2025_26/Studentska.WinApp/frmXO.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Studentska.WinApp
+{
+    partial class frmXO
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            btnReset = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(387, 69);
+            button1.Name = "button1";
+            button1.Size = new Size(119, 97);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(512, 69);
+            button2.Name = "button2";
+            button2.Size = new Size(119, 97);
+            button2.TabIndex = 0;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(637, 69);
+            button3.Name = "button3";
+            button3.Size = new Size(119, 97);
+            button3.TabIndex = 0;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(387, 172);
+            button4.Name = "button4";
+            button4.Size = new Size(119, 97);
+            button4.TabIndex = 0;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(512, 172);
+            button5.Name = "button5";
+            button5.Size = new Size(119, 97);
+            button5.TabIndex = 0;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(637, 172);
+            button6.Name = "button6";
+            button6.Size = new Size(119, 97);
+            button6.TabIndex = 0;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click_1;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(387, 275);
+            button7.Name = "button7";
+            button7.Size = new Size(119, 97);
+            button7.TabIndex = 0;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(512, 275);
+            button8.Name = "button8";
+            button8.Size = new Size(119, 97);
+            button8.TabIndex = 0;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(637, 275);
+            button9.Name = "button9";
+            button9.Size = new Size(119, 97);
+            button9.TabIndex = 0;
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Player X :";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(49, 104);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Player O :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(110, 69);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(110, 104);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 2;
+            label4.Text = "label3";
+            label4.Click += label4_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.BackColor = SystemColors.ActiveCaption;
+            btnReset.Location = new Point(663, 399);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(93, 39);
+            btnReset.TabIndex = 3;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = false;
+            btnReset.Click += btnReset_Click;
+            // 
+            // frmXO
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnReset);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button9);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "frmXO";
+            Text = "frmXO";
+            Load += frmXO_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button btnReset;
+    }
+}
---
+++ cs-programming-2025_26/Studentska.WinApp/frmXO.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Studentska.WinApp
+{
+    public partial class frmXO : Form
+    {
+        public int Brojac { get; set; }
+        private Igrac playerX;
+        private Igrac playerO;
+        private bool krajIgre = false;
+        public frmXO()
+        {
+            InitializeComponent();
+            GenerisiIgrace();
+            UcitajPobjede();
+        }
+
+        private void UcitajPobjede()
+        {
+            label3.Text = $"{playerX.Pobjede}";
+            label4.Text = $"{playerO.Pobjede}";
+
+        }
+
+        private void GenerisiIgrace()
+        {
+            playerX = new Igrac("X");
+            playerO = new Igrac("O");
+        }
+
+        private void frmXO_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            RegistrujPotez(sender);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RegistrujPotez(sender);
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            RegistrujPotez(sender);
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            RegistrujPotez(sender);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistrujPotez(sender);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistrujPotez(sender);
+        }
+
+        public void RegistrujPotez(object sender)
+        {
+            Button button = sender as Button;
+            if (button != null && button.Text == "")
+            {
+                if (Brojac % 2 == 0)
+                {
+                    button.Text = "X";
+                    button.BackColor = Color.LightGray;
+                }
+                else
+                {
+                    button.Text = "O";
+
+                }
+                Brojac++;
+                if (KrajIgre())
+                {
+
+                    var pobjednikSimbol = button.Text;
+
+                    if (pobjednikSimbol == playerX.Simbol)
+                    {
+                        playerX.Pobjede++;
+                    }
+                    else
+                    {
+                        playerO.Pobjede++;
+                    }
+                    UcitajPobjede();
+                    OnemoguciOdabir();
+                    MessageBox.Show($"Winner: {button.Text}");
+                }
+            }
+        }
+
+        public void IgrajPonovo()
+        {
+            Brojac = 0;
+
+            Button[] buttons = { button1, button2, button3, button4, button5, button6, button7, button8, button9 };
+
+            foreach(var button in buttons)
+            {
+                button.Text = "";
+                button.BackColor = Color.White;
+                button.Enabled = true;
+            }
+        }
+        public void OnemoguciOdabir()
+        {
+            Brojac = 0;
+
+            Button[] buttons = { button1, button2, button3, button4, button5, button6, button7, button8, button9 };
+
+            foreach (var button in buttons)
+            {
+                button.Enabled = false;
+            }
+        }
+
+        private bool KrajIgre()
+        {
+            return ProvjeriDijagonale() || ProvjeriRedove() || ProvjeriKolone();
+        }
+
+        private bool ProvjeriKolone()
+        {
+            return ProvjeriPobjedu(button1, button4, button7) || ProvjeriPobjedu(button2, button5, button8) || ProvjeriPobjedu(button3, button6, button9);
+        }
+
+        private bool ProvjeriRedove()
+        {
+            return ProvjeriPobjedu(button1, button2, button3) || ProvjeriPobjedu(button4, button5, button6) || ProvjeriPobjedu(button7, button8, button9);
+        }
+
+        private bool ProvjeriDijagonale()
+        {
+            return ProvjeriPobjedu(button1, button5, button9) || ProvjeriPobjedu(button3, button5, button7);
+        }
+
+        private bool ProvjeriPobjedu(Button button1, Button button2, Button button3)
+        {
+            var pobjeda = button1.Text != "" && button1.Text == button2.Text && button2.Text == button3.Text;
+            if (pobjeda)
+            {
+                button1.BackColor = button2.BackColor = button3.BackColor = Color.LightGreen;
+                return pobjeda;
+            }
+            return false;
+        }
+
+        public class Igrac
+        {
+            public string Simbol { get; set; }
+            public int Pobjede { get; set; }
+
+            public Igrac(string simbol)
+            {
+                Simbol = simbol;
+                Pobjede = 0;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            RegistrujPotez(sender);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RegistrujPotez(sender);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            RegistrujPotez(sender);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            IgrajPonovo();
+        }
+    }
+}
